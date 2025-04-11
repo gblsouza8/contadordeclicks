ba72d06ecb4038ee31fcecbef76b0d36a7744974
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-11 16:28:08 -0300

    Terceiro Commit

--- contadordeclicks/src/main/java/contadordeclicks/Interface.java
+++ contadordeclicks/src/main/java/contadordeclicks/Interface.java
@@ -1,7 +1,9 @@
 package contadordeclicks;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 public class Interface extends JFrame {
+    private int qtd = 0;
     private String quantidade;
     private final JLabel barra; 
 
@@ -11,6 +13,16 @@ public class Interface extends JFrame {
         barra = new JLabel("Clique!");
         add(barra, BorderLayout.SOUTH);
         addMouseListener(new MouseClickHandler());
+    }
 
+    private class MouseCLickHandler extends MouseAdapter
+    {
+        @Override
+        public void mouseClicked(MouseEvent event)
+        {
+            qtd++;
+            quantidade = String.format("Clicou %d vezes", qtd);
+            barra.setText(quantidade);
+        }
     }
 }
