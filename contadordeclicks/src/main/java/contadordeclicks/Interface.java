c64a04e516a34bf745b3aab690dc034166e782c3
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-11 16:37:51 -0300

    Quinto Commit

--- contadordeclicks/src/main/java/contadordeclicks/Interface.java
+++ contadordeclicks/src/main/java/contadordeclicks/Interface.java
@@ -4,25 +4,38 @@ import java.awt.BorderLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 public class Interface extends JFrame {
+    // contador a ser incrementado a cada click
     private int qtd = 0;
+    // variavel que exibirá a mensagem na barra
     private String quantidade;
+    // barra que ficará no canto inferior da tela 
     private final JLabel barra; 
 
+    // construtor
     public Interface()
     {
+        // titulo da janela
         super("Contador de cliques do mouse");
+        // define barra como JLabel e define o texto que ficará até o primeiro clique
         barra = new JLabel("Clique!");
+        // adiciona a barra ao canto inferior da tela
         add(barra, BorderLayout.SOUTH);
+        // adiciona um mouselistener
         addMouseListener(new MouseClickHandler());
     }
 
+    // classe que extende mouseadapter para tratar o evento
     private class MouseClickHandler extends MouseAdapter
     {
+        // sobreescreve mouseClicked
         @Override
         public void mouseClicked(MouseEvent event)
         {
+            // incrementa o contador quando o evento ocorre
             qtd++;
+            // altera a string para atualizar o contador
             quantidade = String.format("Clicou %d vezes", qtd);
+            // altera o texto da JLabel barra
             barra.setText(quantidade);
         }
     }
--- contadordeclicks/src/main/java/contadordeclicks/Main.java
+++ contadordeclicks/src/main/java/contadordeclicks/Main.java
@@ -4,8 +4,12 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         Interface Interface = new Interface();
+
+        // define que o programa se encerrará ao apertar em fechar
         Interface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // define a altura e a largura da janela
         Interface.setSize(200, 75);
+        // define a visibilidade
         Interface.setVisible(true);
         
     }
